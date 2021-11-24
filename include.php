024463ff6073470694fd1a60e6c7c4f05107a1a3
Meoltieki <88784165+Meoltieki@users.noreply.github.com>
2021-11-24 16:33:15 +0100

    yes

--- include.php
+++ include.php
@@ -1,6 +1,6 @@
 
 <link rel = "stylesheet" href = "stylesheet.css">
 
-<div class = header><h1>BLOG</h1> </div>
+
 
 <nav> <a href = "/blog/blog-blj/new.php">New Posts</a></nav></br>
--- new.php
+++ new.php
@@ -1,3 +1,17 @@
+<?php
+$user = 'root';
+$password = '';
+$database = 'blog';
+
+$conn = new PDO('mysql:host=localhost;dbname=' .$database, $user, $password);
+
+$limit = 10;
+$statement = $conn->prepare("SELECT `created_by`, `creadet_at`, `post_title`,`post_text` FROM `posts` ORDER BY `id` DESC LIMIT :limit");
+$statement->bindParam(':limit', $limit, PDO::PARAM_INT);
+$statement->execute();
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,6 +24,7 @@
 
 <body>
 
+<div class = header><h1>BLOG</h1> </div>
 
     <?php include 'include.php' ?>
 

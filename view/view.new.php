5e6e74c67a9e01f499c80b32565ef69d23f2d56c
Meoltieki <88784165+Meoltieki@users.noreply.github.com>
2021-11-26 14:49:32 +0100

    yes

--- view/view.new.php
+++ view/view.new.php
@@ -8,11 +8,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel = "stylesheet" href = "stylesheet.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
     <title>New Posts</title>
 </head>
 
 <body>
-
 <div class = header><h1>BLOG</h1> </div>
 
     <?php include 'include.php'?>
@@ -27,13 +28,25 @@
      <?php foreach ($pdo->query($sql) as $row) { ?>
 
         <div class = "php">
-
            <p><?=  $row['created_by']?> </p><br /> 
            <p> <?=  $row['post_title']?></p> <br /><br />
            <p><?=  $row['post_text']?> </p><br /><br />
-           <p><?= $row ['creadet_at']?></p>
-
+            <p><?= $row ['creadet_at']?></p>
         </div>
+
+
+
+        <form action="new.php" method="POST">
+
+
+            <label for="comment">Comment</label><br />
+
+            <textarea name="comment" id="comment" cols="40" rows="2"><?= ($post ?? '') ?></textarea></br>
+
+
+
+            <input type="submit" value="Comment"></br>
+        </form></br>
 
     <?php } ?>
     
